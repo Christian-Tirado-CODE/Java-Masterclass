a65ee321eb1f1e98a95a28dcf30f9f2c2bf2f873
Christian <tiradoc02@gmail.com>
2022-03-08 08:33:13 -0400

    Access Modifiers

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,38 @@
 package com.company;
 
 
-
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    Account timsAccount = new Account("Tim");
+    timsAccount.deposit(1000);
+    timsAccount.withdraw(500);
+    timsAccount.withdraw(-20);
+    timsAccount.deposit(-20);
+    timsAccount.calculateBalance();
+    //timsAccount.balance = 5000; Error
 
-
-        /*
-         Packages:
-           - Allow you to group classes and interfaces together to prevent or manage naming conflicts between developers.
-           - When importing two classes with the same name you have specify one of them in your imports.
-           -  You use the asterisk symbol(*) when you want to import all the classes and interfaces from a given folder.
-
-         Challenge:
-          Create a suitably named package containing a class called Series with the following static methods:
-          nSum(int n) returns the sum of all numbers from 0 to n. The first ten numbers are:
-         factorial(int n) returns the product of all numbers from 1 to n.
-         fibonacci(int n) returns the nth Fibonacci number.
-
-         When you have tested your functions, delete the Main class and produce a jar file.
-
-         Create a new project and add your Series library, then test the three methods in the main() method of your new project.
-         */
+    System.out.println("Balance on account is " + timsAccount.getBalance());
+    //timsAccount.transactions.add(4500); Error
+    timsAccount.calculateBalance();
     }
+
+    /*
+      NOTES:
+       - We cannot have a private class at the top level.
+       - Only classes, interfaces and enums can exist at the top level.
+
+         Top level:
+            - public: the object is visible to all classes everywhere, whether they are in the same package or have imported the package containing the public class.
+            - package-private: the object is only available within its own package(and is visible to every class within the same package). Package-private is specified by not specifying, i.e it is the default if you do not specify public. There is not a "package-private" keyword.
+         Member level:
+            - public: At the member level, public has the same meaning as at the top level. A public class member(or field) and public method can be accessed from any other class anywhere, even in a different package.
+            - package-private: this also has the same meaning as it does at the top level. An object with no access modifier is visible to every class within the same package(but not to classes in external packages).
+            - private: the object is only visible within the class it is declared. It is not visible anywhere else(incluiding in subclasses of its class).
+            - protected: The object is visible anywhere in its own package(like package-private) but also in subclasses even if they are in another package.
+
+     */
 }
