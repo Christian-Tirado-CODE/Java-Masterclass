39ca0d9410d7a5d98c0ab0287e15c7fe48432c27
Christian <tiradoc02@gmail.com>
2022-03-07 21:14:49 -0400

    Packages

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,29 @@
 package com.company;
 
+
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+
+        /*
+         Packages:
+           - Allow you to group classes and interfaces together to prevent or manage naming conflicts between developers.
+           - When importing two classes with the same name you have specify one of them in your imports.
+           -  You use the asterisk symbol(*) when you want to import all the classes and interfaces from a given folder.
+
+         Challenge:
+          Create a suitably named package containing a class called Series with the following static methods:
+          nSum(int n) returns the sum of all numbers from 0 to n. The first ten numbers are:
+         factorial(int n) returns the product of all numbers from 1 to n.
+         fibonacci(int n) returns the nth Fibonacci number.
+
+         When you have tested your functions, delete the Main class and produce a jar file.
+
+         Create a new project and add your Series library, then test the three methods in the main() method of your new project.
+         */
     }
 }
